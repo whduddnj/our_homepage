937c32f029832f542b36f0b86bef3828c5e588e5
whduddnj <whduddnj@yahoo.co.jp>
2023-08-20 09:55:42 +0900

    add dropdown UI

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const welcomeMessage = "Hello, welcome to my homepage!";
 // alert(welcomeMessage);
 
 console.log("This is a message in the browser's console.");
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
